7c663e239705541282b143e535862d776ff55944
Alejandro Rojas <alejandro.rojas@globant.com>
2024-01-12 10:09:03 -0600

    Deleting vulnerable dependency.

--- src/main/java/com/alejandrorojas/exercises/impl/TrywithResourceTest.java
+++ src/main/java/com/alejandrorojas/exercises/impl/TrywithResourceTest.java
@@ -4,31 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 public class TrywithResourceTest {
-	static Logger logger = Logger.getLogger(TrywithResourceTest.class.getName());
-//	static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(TrywithResourceTest.class);
-	
+
 	public static void main(String[] args) {
-		logger.info("Start main");
 		// It is useful with resources that implement java.lang.AutoCloseable and
 		// java.io.Closeable
 		final String path = "dogsNames.txt";
 			
 		try (FileReader fr = new FileReader(path); BufferedReader br = new BufferedReader(new FileReader(path))) {
-			br.lines().forEach(logger::info);
+			br.lines().forEach(System.out::println);
 
 			//It is not necessary to close (Explicity)
 			br.close();
 			fr.close();
 			
 		} catch (FileNotFoundException e) {
-			logger.severe("FileNotFoundException: "+ e.getLocalizedMessage());
+			System.err.println("FileNotFoundException: "+ e.getLocalizedMessage());
 		} catch (IOException e) {
-			logger.severe("IOException: "+ e);
+			System.err.println("IOException: "+ e);
 		} catch (Exception e) {
-			logger.severe("Exception: "+ e);
+			System.err.println("Exception: "+ e);
 		}
 
 	}
